fe44174fc043818a6f977fcb42372e9ffee2ce35
asavy79 <asavard@uoguelph.ca>
2023-10-10 22:06:38 -0400

    added dad joke generator

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 const getJokeBtn = document.getElementById("getJokeBtn"); // add id
 const jokeText = document.getElementById("jokeText"); // add id
 
-function apiCall() { 
+
+function jokeApiCall() { 
 
     jokeText.textContent = "Fetching a joke...";
 
     /*Step 19: Create account with api-ninjas (https://api-ninjas.com/) */
     /*Step 20: Get API url and API Key */
 
-    fetch("https://api.api-ninjas.com/v1/dadjokes?limit=", { // add event request url
+    fetch("https://api.api-ninjas.com/v1/jokes?limit=1", { // add event request url
         headers: { 'X-Api-Key': 'JFXRiOWn2ELtxCKG9QXPSw==BjL6EMzuSHq6qrmZ' } // add api key
     })
         // Checks the network response 
@@ -34,8 +35,39 @@ function apiCall() {
 };
 
 
+const getDadJokeBtn = document.getElementById("getDadJokeBtn");
+const dadJokeText = document.getElementById("dadJokeText");
+
+function dadJokeApiCall() {
+
+    dadJokeText.textContent = "Fetching a dad joke...";
+
+    fetch("https://api.api-ninjas.com/v1/dadjokes?limit=1", {
+        headers: { 'X-Api-Key': 'JFXRiOWn2ELtxCKG9QXPSw==BjL6EMzuSHq6qrmZ' }
+    })
+
+    .then(function(response) {
+        if (!response.ok) throw new Error("Network response was not ok");
+            return response.json();
+    })
+
+    .then(function(data) {
+        dadJokeText.textContent = data[0].joke;
+    })
+
+    .catch(function(error) {
+        console.error("Error fetching joke:", error);
+        jokeText.textContent = "An error occurred while fetching the joke.";
+    });
+
+
+
+}
+
 /*Step 18: Create event listener for button click */
-getJokeBtn.addEventListener("click", apiCall)
+getJokeBtn.addEventListener("click", jokeApiCall)
+
+getDadJokeBtn.addEventListener("click", dadJokeApiCall)
 
 
 
